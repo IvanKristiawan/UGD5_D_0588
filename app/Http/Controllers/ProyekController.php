f340334901612cd7468baf1a939f703b4b083e46
Ivan Kristiawan <ivanteens@gmail.com>
2022-10-05 17:28:46 +0700

    Tugas6

--- app/Http/Controllers/ProyekController.php
+++ app/Http/Controllers/ProyekController.php
@@ -1,35 +1,5 @@
 <?php
 
-// namespace App\Http\Controllers;
-
-// /* Import Model */
-// use App\Models\Proyek;
-// use App\Models\Departemen;
-// use Illuminate\Http\Request;
-// use DB;
-
-// class ProyekController extends Controller
-// {
-//      /**
-//      * index
-//      * 
-//      * @return void
-//      */
-
-//     public function index()
-//     {
-        // // det posts
-        // $proyek = Proyek::get();
-        // $departemen = Departemen::get();
-        // $proyek = DB::table('proyeks')->paginate(5);
-        // // $departemen = DB::table('departemen')->paginate(10);
-
-        // // render view with posts
-        // return view('proyek.index', compact(['proyek', 'departemen']));
-        // // return view('departemen.index', ['departemen' => $departemen]);
-//     }
-// }
-
 namespace App\Http\Controllers;
 
 /* Import Model */
